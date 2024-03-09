1873e8e6059c2105ba95947bfe8b2e19a8311cc0
Supriyachintha-95 <161014600+Supriyachintha-95@users.noreply.github.com>
2024-03-09 15:43:38 +0530

    Add files via upload

---
+++ text.js
@@ -0,0 +1,16 @@
+console.log("variables and datatypes");
+let num=45;//number
+let num1=34;
+let x="javascript";//string
+let y="yashodha";
+let arr1=[1,2,3];//array
+let bool1=true;//boolean
+console.log("number: "+num);
+console.log("sub :"+x);
+console.log("bool1 :"+bool1 );
+//swapping
+[num,num1]=[num1,num];
+[x,y]=[y,x];
+console.log("swapped string :"+x);
+console.count("swapped number :"+num);
+
